dc5355f4196489170791b359d92b9f600f85373e
Sidow252 <sidow252@gmail.com>
2022-06-09 15:16:04 -0700

    updated

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,4 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
-void positive_or_negative(int i);
+/**
+ * main - check the code.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
