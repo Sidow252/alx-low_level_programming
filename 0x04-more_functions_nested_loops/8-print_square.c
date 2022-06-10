bac88f8a30d2b2c8cddc34feed6800f17565985c
Sidow252 <sidow252@gmail.com>
2022-06-10 12:32:59 -0700

    A function that prints a square, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * main - prints a square of length size in the terminal using #
+ * followed by a new line
+ * Description: If size is 0 or less, the function should print only a new line
+ *
+ **/
+
+void print_square(int size)
+{
+int i;
+
+int j;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+
+_putchar('#');
+
+
+_putchar('\n');
+												}
+
+if (size <= 0)
+_putchar('\n');
+
+}
