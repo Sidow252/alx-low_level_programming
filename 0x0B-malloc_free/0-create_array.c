6d93533bffcf0a4bb2e2e462d5a36eaf604b565d
Sidow252 <sidow252@gmail.com>
2022-06-29 12:07:10 -0700

    A function that creates an array of chars, and initializes it with a specific char.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,67 +1,30 @@
-nclude <stdlib.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-
-
-
 /**
+ *create_array - creates array of chars initializes it to a specific char
+ *@size:intended size
+ *@c:character
  *
- *  * *create_array - creates an array of chars,
- *
- *   * and initializes it with a specific char
- *
- *    * @size: size of the array to create
- *
- *     * @c: char to initialize the array c
- *
- *      *
- *
- *       * Return: pointer to the array (Success), NULL (Error)
- *
- *        */
-
+ *Return:Null,pointer to array
+ */
 char *create_array(unsigned int size, char c)
-
 {
+	unsigned int i;
+	char *p;
 
-		char *p;
-
-			unsigned int i = 0;
-
-
-
-				if (size == 0)
-
-							return (NULL);
-
-
-
-					p = (char *) malloc(sizeof(char) * size);
-
-
-
-						if (p == NULL)
-
-									return (0);
-
-
-
-							while (i < size)
-
-									{
-
-												*(p + i) = c;
-
-														i++;
-
-															}
-
-
-
-								*(p + i) = '\0';
-
-
-
-									return (p);
-
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(*p) * size);
+	if (!p)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		*(p + i) = c;
+	}
+	return (p);
 }
