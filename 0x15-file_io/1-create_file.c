7abf25c58faa3fc716ebb8ac3fd19ed7478041b1
Sidow252 <sidow252@gmail.com>
2022-07-25 14:33:41 -0700

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,4 +38,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-
+	if (text_content == NULL)
+		return (1);
+	fd_write = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	if (fd_write == -1)
+		return (-1);
+	return (1);
+}
